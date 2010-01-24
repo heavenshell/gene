f8302682d16c06f0e74eba307ee025d71023c296
s_ohyanagi <heavenshell.jp@gmail.com>
2010-01-24 13:44:24 +0900

    Fix for tests

--- tests/unit/library/Service/var/Test/Validator.php
+++ tests/unit/library/Service/var/Test/Validator.php
@@ -1,7 +1,7 @@
 <?php
 class Test_Service_Validator extends Gene_Service_Validator
 {
-    protected function _setDefaultRules($session = null, $adapter = null)
+    public function setRules($session = null, $id = null, $adapter = null)
     {
         $validations = array(
             'test1' => array(
@@ -25,6 +25,66 @@ class Test_Service_Validator extends Gene_Service_Validator
         );
         $this->_rules = $validations;
         return $this;
-
     }
+
+    public function createRules()
+    {
+        $validations = array(
+            'name' => array(
+                new Zend_Validate_NotEmpty(),
+                new Zend_Validate_Regex('/^[0-9A-Za-z+\_]*$/ '),
+                new Zend_Validate_StringLength(1, 100, 'utf-8'),
+                'messages' => array(
+                    0 => $this->_translate->_('Please enter user name.'),
+                    1 => $this->_translate->_('Invalid user name.'),
+                    2 => $this->_translate->_('User name should enter less than 100.'),
+                )
+            )
+        );
+        $this->_rules = $validations;
+        return $this;
+    }
+
+    public function updateRules()
+    {
+        $validations = array(
+            'id' => array(
+                new Zend_Validate_Digits(),
+                'messages' => array(
+                    0 => $this->_translate->_('Id should be a number.')
+                )
+            ),
+            'name' => array(
+                new Zend_Validate_NotEmpty(),
+                new Zend_Validate_Regex('/^[0-9A-Za-z+\_]*$/ '),
+                new Zend_Validate_StringLength(1, 100, 'utf-8'),
+                'messages' => array(
+                    0 => $this->_translate->_('Please enter user name.'),
+                    1 => $this->_translate->_('Invalid user name.'),
+                    2 => $this->_translate->_('User name should enter less than 100.'),
+                )
+            )
+        );
+        $this->_rules = $validations;
+        return $this;
+    }
+
+    public function deleteRules()
+    {
+        $validations = array(
+            'id' => array(
+                new Zend_Validate_Digits(),
+                'messages' => array(
+                    0 => $this->_translate->_('Id should be a number.')
+                )
+            )
+        );
+        $this->_rules = $validations;
+        return $this;
+    }
+
+
+
+
+
 }
--- tests/unit/library/Service/var/Test/Zend.php
+++ tests/unit/library/Service/var/Test/Zend.php
@@ -1,19 +1,24 @@
 <?php
 class Test_Service_Zend extends Zend_Db_Table_Abstract
 {
-    /**
-     * Table name
-     *
-     * @var    string
-     * @access protected
-     */
     protected $_name = 'gene_model_test';
 
-    /**
-     * Primary key
-     *
-     * @var    string
-     * @access protected
-     */
     protected $_primary = 'id';
+
+    public function doCreate(array $params)
+    {
+        return $this->insert($params);
+    }
+
+    public function doUpdate(array $params)
+    {
+        $where = array('id = ?' => $params['id']);
+        return $this->update($params, $where);
+    }
+
+    public function doDelete(array $params)
+    {
+        $where = array('id = ?' => $params['id']);
+        return $this->delete($params, $where);
+    }
 }
